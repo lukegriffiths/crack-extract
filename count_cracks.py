df0a92c2da64a1768fc8d6c407119a3d1fe33825
Luke Griffiths <griff.luke@gmail.com>
2019-01-03 23:04:13 +0100

    Initial commit

---
+++ count_cracks.py
@@ -0,0 +1,177 @@
+import numpy as np
+import cv2
+import matplotlib
+from matplotlib import pyplot as plt
+from scipy import ndimage as ndi
+from matplotlib import cm
+
+plt.close('all')
+
+""" Plot parameters """
+rcParams = matplotlib.rcParams
+rcParams['svg.fonttype'] = 'none'
+rcParams['font.sans-serif'] = ['Arial']
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.size'] = '8'
+rcParams['legend.numpoints'] = 1
+
+""" Import and skeletonise image """
+filename = './output/img11.png'
+img = cv2.imread(filename, 0)  # Load crack image
+
+# img = np.uint8( scipy.misc.imread(filename, flatten = True) )
+img[img > 0] = 255
+img_orig = img
+# img = img[0:2000, 0:2000]
+
+res = np.floor(1000 * 261. / 526)  # Pixels per mm
+img_height = img.shape[0]
+img_width = img.shape[1]
+area = (img_height / res) * (img_width / res)  # Area of image in mm
+
+plt.figure()
+plt.imshow(img, cmap='gray', interpolation='none')  # Show image
+plt.xticks([]), plt.yticks([])  # To hide tick values on X and Y axis
+plt.tight_layout()
+plt.show()
+
+""" Build Grids """
+large_gridsize_mm = 1  # Large grid size in mm
+small_gridsize_mm = 0.1  # Fine grid size in mm
+small_gridsize = np.ceil(small_gridsize_mm * res)  # Number of pixels to cover small grid element
+large_gridsize = np.ceil(
+    small_gridsize * large_gridsize_mm / small_gridsize_mm)  # Number of pixels to cover large grid element
+grid_hor = np.uint8(np.zeros(img.shape))  # Create table same size as image
+grid_ver = np.uint8(np.zeros(img.shape))
+grid_hor[::small_gridsize, :] = 255  # Set horizontal lines white
+grid_ver[:, ::small_gridsize] = 255  # Set vertical lines white
+large_grid_hor = np.uint8(np.zeros(img.shape))  # Create table same size as image
+large_grid_ver = np.uint8(np.zeros(img.shape))
+large_grid_hor[::large_gridsize, :] = 255  # Set horizontal lines white
+large_grid_ver[:, ::large_gridsize] = 255  # Set vertical lines white
+
+""" Plot images with grid overlain"""
+small_grid = grid_hor + grid_ver
+small_grid[small_grid > 0] = 255
+large_grid = large_grid_hor + large_grid_ver
+large_grid[large_grid > 0] = 255
+
+filename = './output/img13.png'
+img_labelled = matplotlib.image.imread(filename)  # Load crack image
+
+img_labelled[:, :, 0][small_grid == 255] = 150
+img_labelled[:, :, 1][small_grid == 255] = 150
+img_labelled[:, :, 2][small_grid == 255] = 150
+img_labelled[:, :, 0][large_grid == 255] = 1
+img_labelled[:, :, 1][large_grid == 255] = 1
+img_labelled[:, :, 2][large_grid == 255] = 1
+
+fig = plt.figure(figsize=(9 / 2.54, 5 / 2.54))
+ax = fig.add_subplot(1, 1, 1)  # Show image, grids and intersections
+plt.imshow(img_labelled, interpolation='nearest')  # Show image
+plt.xticks([]), plt.yticks([])  # To hide tick values on X and Y axis
+plt.tight_layout()
+plt.savefig('./output/test.png')
+plt.show()
+
+matplotlib.image.imsave('GGGIntact2_cracks_labelled_grid.png', img_labelled)
+
+""" Calculate crack properties over entire image """
+
+int_hor = np.zeros_like(img)  # Black image
+int_hor[np.logical_and(img == 255, grid_hor == 255)] = 255  # Where img and grid interect, color intersections white
+int_ver = np.zeros_like(img)  # Black image
+int_ver[np.logical_and(img == 255, grid_ver == 255)] = 255
+cracks, nb_hor = ndi.label(int_hor)  # Number of horizontal intersections
+cracks, nb_ver = ndi.label(int_ver)  # Number of vertical intersections
+cracks, nb = ndi.label(img)  # Label all cracks
+Na = nb / area  # Cracks per surface
+slices = ndi.find_objects(cracks)  # Find the locations of all objects
+length = np.zeros([nb, 1])  # Will contain all crack lengths
+for k in np.arange(0, len(slices)):  # Go through each cluster
+    [crack_width, crack_height] = img[slices[k]].shape
+    length[k] = np.sqrt(crack_width ** 2 + crack_height ** 2) / res  # Crack length in mm as if straight
+
+average_length = np.mean(length)  # Calculate mean crack length
+
+PI = nb_hor / ((img_width / res) * (
+            img_height / small_gridsize))  # nb. of intersections with horizontal / ( nb. of lines * line length )
+PII = nb_ver / ((img_height / res) * (
+            img_width / small_gridsize))  # nb. of intersections with vertical lines / ( nb. of lines * line length )
+Sv = PI + PII  # Underwood 1980
+
+# Print results
+print('### Crack properties over entire image ### \n')
+print('nb hor = ' + str(nb_hor) + ', nb_ver = ' + str(nb_ver))
+print('Large gridsize = ' + str(large_gridsize_mm) + ' mm')
+print('Fine gridsize = ' + str(small_gridsize_mm) + ' mm')
+print('PI = ' + str(PI) + ', PII = ' + str(PII) + ', Sv = ' + str(Sv))  # Print results
+print('Walsh crack density Na (number/area) = ' + str(Na) + ' mm^-2')
+print('Average crack length = ' + str(average_length) + ' mm')
+print('Calculated D0 from Sv = ' + str(Sv * np.pi ** 2 * average_length / 32.))
+print('Calculated D0 from Na = ' + str(Na * (average_length / 2) ** 2 * np.pi / 2))
+
+""" Calculate crack properties in patches """
+
+crack_density = np.zeros(
+    [np.ceil(img_height / large_gridsize), np.ceil(img_width / large_gridsize)])  # Nb. of elements = nb. of patches
+crack_length = np.zeros([np.ceil(img_height / large_gridsize), np.ceil(img_width / large_gridsize)])
+Na = np.zeros([np.ceil(img_height / large_gridsize), np.ceil(img_width / large_gridsize)])
+PI = np.zeros([np.ceil(img_height / large_gridsize), np.ceil(img_width / large_gridsize)])
+PII = np.zeros([np.ceil(img_height / large_gridsize), np.ceil(img_width / large_gridsize)])
+
+""" Compare image and grid """
+for i in range(crack_density.shape[0]):
+    for j in range(crack_density.shape[1]):
+        iMin = i * (large_gridsize)
+        iMax = min((i + 1) * large_gridsize, img.shape[0])
+        jMin = j * (large_gridsize)
+        jMax = min((j + 1) * large_gridsize, img.shape[1])
+        img_section = img[iMin:iMax, jMin:jMax]
+        int_hor = np.zeros_like(img_section)  # Black image
+        int_hor[np.logical_and(img_section == 255, grid_hor[iMin:iMax,
+                                                   jMin:jMax] == 255)] = 255  # Where img and grid interect, color intersections white
+        int_ver = np.zeros_like(img_section)  # Black image
+        int_ver[np.logical_and(img_section == 255, grid_ver[iMin:iMax, jMin:jMax] == 255)] = 255
+        cracks, nb_hor = ndi.label(int_hor)  # Number of horizontal intersections
+        cracks, nb_ver = ndi.label(int_ver)  # Number of vertical intersections
+        cracks, nb = ndi.label(img_section)  # Number of cracks in patch
+        height = img_section.shape[0]
+        width = img_section.shape[1]
+        area = (height / res) * (width / res)
+        Na[i, j] = nb / area
+        slices = ndi.find_objects(cracks)  # Find the locations of all objects
+        length = np.zeros([nb, 1])
+        for k in np.arange(0, len(slices)):  # Go through each cluster
+            [crack_width, crack_height] = img_section[slices[k]].shape
+            length[k] = np.sqrt(crack_width ** 2 + crack_height ** 2)
+        #       plt.figure() # Show image, grids and intersections
+        #       plt.imshow(int_ver + int_hor,  interpolation='nearest') # Show image
+        #       plt.xticks([]), plt.yticks([])  # To hide tick values on X and Y axis
+        #       plt.tight_layout()
+        #       plt.show()
+        crack_length[i, j] = np.mean(length) / res
+        PI[i, j] = nb_hor / ((width / res) * (height / small_gridsize))
+        PII[i, j] = nb_ver / ((height / res) * (width / small_gridsize))
+
+Sv = PI + PII
+
+print('\n \n ### Average crack properties over patched image ### \n')
+print('nb hor = ' + str(np.mean(nb_hor)) + ', nb_ver = ' + str(np.mean(nb_ver)))
+print('Large gridsize = ' + str(large_gridsize_mm) + ' mm')
+print('Fine grid size = ' + str(small_gridsize_mm) + ' mm')
+print('PI = ' + str(np.mean(PI)) + ', PII = ' + str(np.mean(PII)) + ', Sv = ' + str(np.mean(Sv)))  # Print results
+print('Walsh crack density Na (number/area) = ' + str(np.mean(Na)) + ' mm^-2')
+print('Average crack length = ' + str(np.mean(crack_length)) + ' mm')
+print('Calculated D0 from Sv = ' + str(np.mean(Sv) * np.pi ** 2 * np.mean(crack_length) / 32.))
+print('Calculated D0 from Na = ' + str(np.mean(Na) * (np.mean(crack_length) / 2) ** 2 * np.pi / 2))
+
+fig = plt.figure(figsize=(8 / 2.54, 7 / 2.54))  # Show image, grids and intersections
+ax = plt.imshow(Sv, extent=[0, img_width / res, 0, img_height / res], cmap=cm.YlOrRd)  # Show image
+plt.xlabel('mm')
+plt.ylabel('mm')
+cb = plt.colorbar(ax, orientation='horizontal')
+cb.set_label('Sv (mm2/mm3)')
+plt.tight_layout()
+plt.savefig('./output/Sv.svg', dpi=600)
+plt.show()
---
+++ extract_cracks.py
@@ -0,0 +1,348 @@
+# %% Imports
+import numpy as np
+import copy
+import cv2
+from matplotlib import pyplot as plt
+from skimage.morphology import watershed, binary_dilation, binary_erosion
+from scipy import ndimage as ndi
+from skimage import filters
+import mahotas as mh
+import matplotlib as mpl
+from PIL import Image
+
+# %% Parameters
+# Color map
+cmap = plt.cm.YlOrRd  # colormap for crack lengths
+cmap.set_bad(color='black')  # set nan colour to black
+
+""" Plot parameters """
+rcParams = mpl.rcParams
+rcParams['svg.fonttype'] = 'none'
+rcParams['font.sans-serif'] = ['Arial']
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.size'] = '8'
+rcParams['legend.numpoints'] = 1
+
+
+# %% Functions
+def branchPoints(img):
+    img[img > 0] = 1  # mh works with binary images
+    branch_points = np.zeros_like(img)
+    bp = []
+    bp.append(np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]]))
+    bp.append(np.array([[1, 0, 1], [0, 1, 0], [1, 0, 1]]))
+    bp.append(np.array([[2, 1, 2], [1, 1, 1], [0, 0, 0]]))
+    bp.append(np.array([[1, 2, 1], [2, 1, 0], [1, 0, 0]]))
+    bp.append(np.array([[2, 1, 0], [1, 1, 0], [2, 1, 0]]))
+    bp.append(np.array([[1, 0, 0], [2, 1, 0], [1, 2, 1]]))
+    bp.append(np.array([[0, 0, 0], [1, 1, 1], [2, 1, 2]]))
+    bp.append(np.array([[0, 0, 1], [0, 1, 2], [1, 2, 1]]))
+    bp.append(np.array([[0, 1, 0], [0, 1, 1], [0, 1, 0]]))
+    bp.append(np.array([[1, 0, 1], [0, 1, 0], [0, 0, 1]]))
+    bp.append(np.array([[1, 0, 1], [0, 1, 0], [2, 1, 2]]))
+    bp.append(np.array([[0, 1, 0], [1, 1, 2], [0, 2, 1]]))
+    bp.append(np.array([[1, 0, 2], [0, 1, 1], [1, 0, 2]]))
+    bp.append(np.array([[1, 0, 2], [0, 1, 1], [1, 0, 2]]))
+    bp.append(np.array([[0, 2, 1], [1, 1, 2], [0, 1, 0]]))
+    bp.append(np.array([[2, 1, 2], [0, 1, 0], [1, 0, 1]]))
+    bp.append(np.rot90(bp[-1]))  # Rotation of previous array
+    bp.append(np.rot90(bp[-1]))
+    bp.append(np.rot90(bp[-1]))
+    bp.append(np.array([[1, 0, 2], [1, 1, 1], [0, 1, 0]]))
+    bp.append(np.rot90(bp[-1]))
+    bp.append(np.rot90(bp[-1]))
+    bp.append(np.rot90(bp[-1]))
+    bp.append(np.array([[0, 1, 2], [1, 1, 1], [1, 0, 0]]))
+    bp.append(np.rot90(bp[-1]))
+    bp.append(np.rot90(bp[-1]))
+    bp.append(np.rot90(bp[-1]))
+    bp.append(np.array([[1, 1, 0], [0, 1, 1], [0, 1, 0]]))
+    bp.append(np.rot90(bp[-1]))
+    bp.append(np.rot90(bp[-1]))
+    bp.append(np.rot90(bp[-1]))
+
+    for i in np.arange(0, len(bp)):
+        branch_points = branch_points + mh.morph.hitmiss(img, bp[i])
+
+    branch_points[branch_points > 1] = 1  # Remove double points
+    return branch_points
+
+
+def endPoints(skel):
+    skel[skel > 0] = 1  # mh works with binary images
+    endpoint1 = np.array([[0, 0, 0], [0, 1, 0], [2, 1, 2]])
+    endpoint2 = np.array([[0, 0, 0], [0, 1, 2], [0, 2, 1]])
+    endpoint3 = np.array([[0, 0, 2], [0, 1, 1], [0, 0, 2]])
+    endpoint4 = np.array([[0, 2, 1], [0, 1, 2], [0, 0, 0]])
+    endpoint5 = np.array([[2, 1, 2], [0, 1, 0], [0, 0, 0]])
+    endpoint6 = np.array([[1, 2, 0], [2, 1, 0], [0, 0, 0]])
+    endpoint7 = np.array([[2, 0, 0], [1, 1, 0], [2, 0, 0]])
+    endpoint8 = np.array([[0, 0, 0], [2, 1, 0], [1, 2, 0]])
+    ep1 = mh.morph.hitmiss(skel, endpoint1)
+    ep2 = mh.morph.hitmiss(skel, endpoint2)
+    ep3 = mh.morph.hitmiss(skel, endpoint3)
+    ep4 = mh.morph.hitmiss(skel, endpoint4)
+    ep5 = mh.morph.hitmiss(skel, endpoint5)
+    ep6 = mh.morph.hitmiss(skel, endpoint6)
+    ep7 = mh.morph.hitmiss(skel, endpoint7)
+    ep8 = mh.morph.hitmiss(skel, endpoint8)
+    ep = ep1 + ep2 + ep3 + ep4 + ep5 + ep6 + ep7 + ep8
+    ep[ep > 1] = 1  # Remove double points
+    return ep
+
+
+def removeSmallObjects(img, size):
+    label_objects, nb_labels = ndi.label(img, [[0, 1, 0], [1, 1, 1], [0, 1, 0]])
+    slices = ndi.find_objects(label_objects)  # Find the locations of all objects
+    sizes = np.zeros([nb_labels + 1, 1])
+    for i in np.arange(0, len(slices)):  # Go through each cluster
+        sizes[i] = np.sqrt(img[slices[i]].shape[0] ** 2 + img[slices[i]].shape[1] ** 2)
+    mask_size = np.where(sizes > size)[0] + 1
+    max_index = np.zeros([nb_labels + 1, 1], np.uint8)
+    max_index[mask_size] = 1
+    max_index = np.squeeze(max_index)
+    label_objects = max_index[label_objects]
+    img = 255 * np.uint8(label_objects)
+    return img
+
+
+def removeEndPoints(skeleton, n):  # Iteratively removes end points n times
+    for i in np.arange(0, n):
+        ep = endPoints(skeleton)  # Find end points
+        skeleton = np.logical_and(skeleton, np.logical_not(ep))  # Skeleton without end points
+        skeleton = 255 * np.uint8(skeleton)  # Skeleton in standard format
+    return skeleton
+
+
+def removeBranchPoints(skeleton):  # Removes branch points
+    branch_points = branchPoints(skeleton)  # Find end points
+    skeleton = np.logical_and(skeleton, np.logical_not(branch_points))  # Skeleton without end points
+    skeleton = 255 * np.uint8(skeleton)  # Skeleton in standard format
+    return skeleton
+
+
+def restoreBranches(skeleton, skeleton_original):  #
+    size_pre = 0
+    i = 0
+    while size_pre != np.sum(
+            skeleton):  # Keeps going until the two consecutive iterations of skeleton have the same sum
+        i = i + 1;
+        # mpl.image.imsave(str(i) + '_prune.png', skeleton[532:555, 560:595], cmap = 'gray')
+        size_pre = np.sum(skeleton)  # Number of white pixels
+        end_points_dilated = 255 * np.uint8(binary_dilation(endPoints(skeleton)))  # Dilate end points
+        skeleton_dilated = 255 * np.uint8(np.logical_or(skeleton, end_points_dilated))  # Add to pruned image
+        # mpl.image.imsave(str(i) + '_prune_dilated.png', skeleton_dilated[532:555, 560:595], cmap = 'gray')
+        skeleton = 255 * np.uint8(
+            np.logical_and(skeleton_dilated, skeleton_original))  # Intersect dilated and unpruned image
+        # mpl.image.imsave(str(i) + '_prune_dilated_masked.png', skeleton[532:555, 560:595], cmap = 'gray')
+        bp = branchPoints(skeleton)  # Find branch points in lengthened image
+        skeleton = removeBranchPoints(skeleton)  # Remove the branchpoints
+        # mpl.image.imsave(str(i) + '_prune_dilated_lone.png', skeleton[532:555, 560:595], cmap = 'gray')
+        skeleton = removeSmallObjects(skeleton, 2)  # Remove lone pixels left by branchpoint removal
+        # mpl.image.imsave(str(i) + '_prune_nobranch.png', skeleton[532:555, 560:595], cmap = 'gray')
+        skeleton = 255 * np.uint8(np.logical_or(skeleton, bp))  # Restore branchpoints
+    skeleton = np.uint8(removeBranchPoints(skeleton))  # Keep cracks separate
+    skeleton = removeSmallObjects(skeleton, 2)  # Remove lone pixels left by branchpoint removal
+    return skeleton
+
+
+def histEq(img, nbr_bins=256):
+    img_hist, bins = np.histogram(img.flatten(), nbr_bins, normed=True)  # Make histogram
+    cdf = img_hist.cumsum()
+    cdf = 255 * cdf / cdf[-1]  # Normalize by highest value
+    img_eq = np.floor(np.interp(img.flatten(), bins[:-1], cdf))  # Interpolate image to new normalised distribution
+    return img_eq.reshape(img.shape)
+
+
+def RDP(img, angle):
+    criticalRatio = 2 * np.tan(np.deg2rad(90 - angle / 2))
+    size_pre = -1
+    new_bp = np.zeros_like(img)  # New branchpoints/crack intersections
+    j = 0
+    while size_pre != np.sum(new_bp):  # Until no more branchpoints are being made
+        j = j + 1
+        size_pre = np.sum(new_bp)
+        label_objects, nb_labels = ndi.label(img, [[0, 1, 0], [1, 1, 1], [0, 1, 0]])  # Find all features
+        slices = ndi.find_objects(label_objects)  # Find the locations of features
+        for i in np.arange(0, len(slices)):  # Go through each clusterdf
+            padded_crack = np.lib.pad(label_objects[slices[i]], 1, 'constant',
+                                      constant_values=0)  # Pad the slice to be able to detect edge points
+            padded_crack[padded_crack != i + 1] = 0  # Remove other cracks in slice
+            endpoints = endPoints(padded_crack)  # Find end points
+            ep = np.column_stack(np.where(endpoints > 0))  # Find coordinates of end points
+            if ep.size > 0:
+                line_length = np.sqrt(
+                    (ep[0, 0] - ep[1, 0]) ** 2 + (ep[0, 1] - ep[1, 1]) ** 2)  # Calculate length of approx. line
+                crack_coords = np.column_stack(np.where(padded_crack > 0))  # Find coordinates of crack
+                distance = np.abs(
+                    (ep[1, 1] - ep[0, 1]) * crack_coords[:, 0] - (ep[1, 0] - ep[0, 0]) * crack_coords[:, 1] + ep[1, 0] *
+                    ep[0, 1] - ep[1, 1] * ep[0, 0]) / line_length
+                if line_length / np.max(distance) <= criticalRatio:
+                    bp_coords = crack_coords[np.argmax(distance),
+                                :] - 1  # New branch point coords. -1 because crack coords is for padded array
+                    bp = np.zeros_like(img[slices[i]])
+                    bp[bp_coords[0], bp_coords[1]] = 255
+                    new_bp[slices[i]] = bp
+        img[new_bp > 0] = 0
+    return img
+
+
+def fillSmallHoles(img, iterations):
+    for i in np.arange(0, iterations):
+        img = binary_dilation(img)  # Dilate and erode to fill small holes in cracks
+    for i in np.arange(0, iterations):
+        img = binary_erosion(img)
+    return img
+
+
+def removeEndPointsIter(img):
+    label_objects, nb_labels = ndi.label(img, [[0, 1, 0], [1, 1, 1], [0, 1, 0]])  # Find all cracks
+    slices = ndi.find_objects(label_objects)  # Find the locations of all cracks
+    new_img = np.zeros_like(label_objects)
+    for i in np.arange(0, len(slices)):  # Go through each crack
+        crack = copy.copy(label_objects[slices[i]])  # Isolate crack slice
+        padded_crack = np.lib.pad(crack, 1, 'constant',
+                                  constant_values=0)  # Pad the slice to be able to detect edge points
+        padded_crack[padded_crack != i + 1] = 0  # Remove any other cracks in slice
+        nb_end_points = np.sum(endPoints(padded_crack))  # Find end points
+        while (nb_end_points > 2):  # While the crack has more than 2 end points
+            padded_crack = removeEndPoints(padded_crack, 1)  # Remove end points
+            nb_end_points = np.sum(endPoints(padded_crack))  # Recalculate number of endpoints
+            print(nb_end_points)
+        new_img[slices[i]] = 255 * np.uint8(np.logical_or(new_img[slices[i]], padded_crack[1:-1, 1:-1]))
+    return new_img
+
+
+def orderByCrackLength(img, res):  # res in pix/mm
+    label_objects, nb_labels = ndi.label(img, [[0, 1, 0], [1, 1, 1], [0, 1, 0]])  # Find all cracks
+    slices = ndi.find_objects(label_objects)  # Find the locations of all objects
+    lengths = np.zeros([nb_labels, 1])
+    for i in np.arange(0, len(slices)):  # Go through each cluster
+        lengths[i] = np.sqrt(img[slices[i]].shape[0] ** 2 + img[slices[i]].shape[1] ** 2)
+    for i in np.arange(1, nb_labels + 1):
+        label_objects[label_objects == i] = -lengths[i - 1]  # Take negative to overwriting other cracks
+    label_objects = -label_objects / res  # FLip back to positive values
+    return label_objects
+
+
+def save_image(img, filepath=None, cmap='gray'):
+    save_image.counter += 1  # increment each time function is called
+
+    if filepath is None:
+        img = Image.fromarray(np.asarray(np.clip(img, 0, 255),
+                                         dtype="uint8"), "L")
+        filepath = './output/img' + str(save_image.counter) + '.jpg'
+        img.save(filepath)
+    else:
+        img = Image.fromarray(np.asarray(np.clip(img, 0, 255),
+                                         dtype="uint8"), "L")
+        img.save(filepath)
+
+
+def processImage(filename):
+    # img_orig = np.uint8( scipy.misc.imread(filename, flatten = True) )
+
+    image = Image.open(filename).convert('L')
+    img_orig = np.uint8(image)
+
+    # img = img[2:-2, 2:-2] # Microscope image borders are all same grayscale
+    # img = img[760:780, 923:977]
+
+    """ Filter image """
+    img = (255 - img_orig)  # Switch grayscale
+    img_median = ndi.median_filter(img, 15)  # Median filter, good at conserving edges
+    img_filtered = (255 - cv2.subtract(img, img_median))  # Subtract filtered image from original (cracks = white)
+
+    """ Segmentation """
+    markers = np.zeros_like(img_filtered)  # Mark the different regions of the image
+    markers[img_filtered > 250] = 1  # Background grayscales
+    markers[img_filtered < 245] = 2  # Crack grayscales
+    elevation_map = filters.sobel(img_filtered)  # Edge detection for watershed
+    segmented = 255 - 255 * watershed(elevation_map, markers)  # Watershed segmentation, white on black
+
+    """ Thin, prune and label cracks """
+    cracks = removeSmallObjects(segmented, 40)  # Remove small objects
+    cracks = fillSmallHoles(cracks, 2)  # Fill small holes in cracks
+    cracks = binary_dilation(cracks)  # Dilate before thinning
+    cracks_skeleton = 255 * np.int8(mh.thin(cracks > 0))  # Skeletonise image
+    cracks_skeleton_pruned = removeEndPoints(cracks_skeleton, 15)  # Remove skeletonisation artefacts
+    cracks_skeleton_pruned_no_bp = removeBranchPoints(cracks_skeleton_pruned)  # Remove branch points to separate cracks
+    cracks_skeleton_pruned_no_bp_2_ep = removeEndPointsIter(
+        cracks_skeleton_pruned_no_bp)  # Remove end points until 2 per crack
+    cracks_skeleton_restored = restoreBranches(cracks_skeleton_pruned_no_bp_2_ep,
+                                               cracks_skeleton)  # Restore branches without creating new endpoints
+
+    """ Save images """
+    save_image.counter = 0
+    save_image(img_orig)
+    save_image(255 - img_median)
+    save_image(255 - img_filtered)
+    save_image(elevation_map)
+    save_image(segmented)
+    save_image(cracks)
+    save_image(cracks_skeleton)
+    save_image(cracks_skeleton_pruned)
+    save_image(cracks_skeleton_pruned_no_bp)
+    save_image(cracks_skeleton_pruned_no_bp_2_ep)
+    save_image(cracks_skeleton_restored)
+
+    return cracks_skeleton_restored
+
+""" Run program """
+
+# image file path
+filename = '../input/GGGIntact2_orig.png'
+
+# process image
+final = processImage(filename)
+
+# Resolution of image in pixels per mm
+res = np.floor(1000 * 261. / 526)
+# image height in pixels
+img_height = final.shape[0]
+# image width in pixels
+img_width = final.shape[1]
+
+# calculate and order by crack length
+crack_length = orderByCrackLength(final, res)  # Calculate crack lengths
+
+# save image
+mpl.image.imsave('./output/img12.png', crack_length, cmap=cmap)
+crack_length = np.ma.masked_where(crack_length == 0, crack_length)  # Mask background
+
+# plot figure of
+fig = plt.figure(figsize=(8 / 2.54, 8 / 2.54))  # Show image
+res = np.floor(1000 * 261. / 526)  # Pixels per mm
+ax = plt.imshow(crack_length, extent=[0, img_width / res, 0, img_height / res], cmap=cmap)  # Show image
+plt.xlabel('mm')
+plt.ylabel('mm')
+cb = plt.colorbar(ax, orientation='horizontal')
+cb.set_label('Crack length (mm)')
+plt.tight_layout()
+plt.savefig('./output/crack_length.png', dpi=600)
+plt.show()
+
+# Image and average deviation angle
+cut = RDP(final, 70)
+
+# Calculate crack lengths
+crack_length_cut = orderByCrackLength(cut, res)
+#  Mask background
+crack_length_cut = np.ma.masked_where(crack_length_cut == 0, crack_length_cut)
+
+#mpl.image.imsave('img13.png', crack_length_cut, cmap=cmap)
+save_image(crack_length_cut, cmap=cmap)
+
+# make figure showing crack lengths
+fig = plt.figure(figsize=(8 / 2.54, 8 / 2.54))  # Show image
+res = np.floor(1000 * 261. / 526)  # Pixels per mm
+img_height = crack_length_cut.shape[0]
+img_width = crack_length_cut.shape[1]
+ax = plt.imshow(crack_length_cut, extent=[0, img_width / res, 0, img_height / res], cmap=cmap)  # Show image
+plt.xlabel('mm')
+plt.ylabel('mm')
+cb = plt.colorbar(ax, orientation='horizontal')
+cb.set_label('Crack length (mm)')
+plt.tight_layout()
+plt.savefig('./output/crack_length_cut.png', dpi=600)
+plt.show()
