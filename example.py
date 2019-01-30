1dc10516afae2665f6533042c2a5bc1df76e037a
Luke Griffiths <griff.luke@gmail.com>
2019-01-30 10:19:27 +0100

    Updated example.

--- example.py
+++ example.py
@@ -1,9 +1,10 @@
 import numpy as np
 import extract_cracks
+import count_cracks
 import matplotlib.pyplot as plt
 
 # Change filepath to use your own image
-filepath = './input/GGGIntact2_orig.png'
+filepath = './input/example_image.png'
 filepath = './input/B-2.tif'
 
 # Resolution of the image in pixels per mm
@@ -11,10 +12,10 @@ resolution = np.floor(1000 * 261. / 526)
 
 # Process image
 final = extract_cracks.processImage(filepath,
-                                    median_filter_size=18,
+                                    median_filter_size=15,
                                     small_object_size=30,
-                                    fill_small_holes_n_iterations=2,
-                                    n_prune=15,
+                                    fill_small_holes_n_iterations=4,
+                                    n_prune=20,
                                     bg_greyscale=250,
                                     crack_greyscale=245)
 
@@ -51,3 +52,19 @@ cb.set_label('Crack length (mm)')
 plt.tight_layout()
 plt.savefig('./output/crack_lengths.png', dpi=600)
 plt.show()
+
+
+# Analyse image of cracks
+Sv, PI, PII, Na, crack_length = count_cracks.calculate_crack_statistics('./output/img11.jpg', resolution=resolution)
+
+# Plot Sv (crack surface density)
+count_cracks.plot_crack_statistics(Sv, img_width, img_height, resolution, colorbar_label='Sv [mm2/mm3]',
+                                   output_filepath='./output/Sv.png')
+
+# Plot Na (number of cracks per unit area)
+count_cracks.plot_crack_statistics(Na, img_width, img_height, resolution, colorbar_label='Na [/mm2])',
+                                   output_filepath='./output/Na.png')
+
+# Plot crack length (endpoint-to-endpoint)
+count_cracks.plot_crack_statistics(crack_length, img_width, img_height, resolution, colorbar_label='Crack length [mm])',
+                                   output_filepath='./output/crack_length.png')
